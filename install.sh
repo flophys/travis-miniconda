953c72180a8388b280c0ee90709ac9131a69ee7b
Danilo Horta <danilo.horta@gmail.com>
2016-07-03 21:54:44 +0100

    conda

--- install.sh
+++ install.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+else
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
+bash miniconda.sh -b -p $HOME/miniconda
 
-export PATH=$HOME/miniconda3/bin:$PATH
-echo "export PATH=\$HOME/miniconda3/bin:\$PATH" >> ~/.bashrc
+export PATH="$HOME/miniconda/bin:$PATH"
+echo "export PATH=\"\$HOME/miniconda/bin:\$PATH\"" >> ~/.bashrc
+hash -r
 
-export CONDA_INSTALL="conda install --yes -q"
-echo "export CONDA_INSTALL=\"conda install --yes -q\"" >> ~/.bashrc
-
-conda create -n travisci --yes python=$PYTHON
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create -q -n travisci python=$TRAVIS_PYTHON_VERSION numba numpy scipy pip sphinx sphinx_rtd_theme pygments pytest
 source activate travisci
+conda install --yes -q -c numba llvmdev=3.7.1
 
-$CONDA_INSTALL -c numba llvmdev=3.7.1
-$CONDA_INSTALL numba numpy scipy pip sphinx sphinx_rtd_theme pygments pytest
-
-if [ $PYTHON \< "3.4" ];
+if [ $TRAVIS_PYTHON_VERSION \< "3.4" ];
 then
-    $CONDA_INSTALL enum34;
+    conda install --yes -q enum34;
 fi
