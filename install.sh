b5b803971cac5449c4c1c9a9e54cf0e6c600ccbd
Danilo Horta <danilo.horta@gmail.com>
2016-07-03 21:34:18 +0100

    missing wget

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 PYTHON=$1
 
-https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b
 
