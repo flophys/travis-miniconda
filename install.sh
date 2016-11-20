605df5f4ccbaf3f52560f1277054d4935907fb31
Danilo Horta <danilo.horta@gmail.com>
2016-11-20 15:13:27 +0000

    cleaning

--- install.sh
+++ install.sh
@@ -20,13 +20,15 @@ pushd .
 cd
 mkdir -p download
 cd download
-echo "Cached in $HOME/download :"
+echo "Cached in $HOME/download:"
 ls -l
 echo
+
 if [[ ! -f miniconda-$infix.sh ]]
 then
     wget $url -O "miniconda-$infix.sh"
 fi
+
 chmod +x miniconda-$infix.sh && ./miniconda-$infix.sh -b -p $HOME/miniconda
 cd ..
 export PATH="$HOME/miniconda/bin:$PATH"
@@ -35,20 +37,23 @@ popd
 hash -r
 
 conda config --set always_yes yes --set changeps1 no
+conda install -n root _license -q
 conda update -q conda
 conda info -a
+
 conda create -q -n travisci python=$TRAVIS_PYTHON_VERSION nomkl numba numpy \
                             scipy pip sphinx sphinx_rtd_theme pygments \
                             pytest psutil pandas h5py hdf5 matplotlib \
-                            pycparser
+                            pycparser cffi llvmlite enum34
 source activate travisci
-conda install --yes -q -c numba llvmdev="3.8*" llvmlite
-conda install --yes -q -c conda-forge pypandoc
 
-if [ $TRAVIS_PYTHON_VERSION \< "3.4" ];
-then
-    conda install --yes -q enum34;
-fi
+# conda install --yes -q -c numba llvmdev="3.8*" llvmlite
+# conda install --yes -q -c conda-forge pypandoc
+
+# if [ $TRAVIS_PYTHON_VERSION \< "3.4" ];
+# then
+#     conda install --yes -q enum34;
+# fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]];
 then
@@ -57,6 +62,5 @@ then
     mkdir -p $HOME/.matplotlib
     touch $HOME/.matplotlib/matplotlibrc
     echo "backend: TkAgg" >> $HOME/.matplotlib/matplotlibrc
-    brew install pandoc
+    # brew install pandoc
 fi
-
